8056f36f598d64f26fe617bdf8921c622d77a4d7
c8ler <kotovsergey@yandex.ru>
2022-05-16 00:00:06 +0300

    DZ-2

---
+++ Task_13/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру 
+// заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+string num = Console.ReadLine();
+if (num.Length >= 3) Console.WriteLine(num[2]);
+else Console.WriteLine("третьей цифры нет");
---
+++ Task_15/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+if (num == 6 || num == 7) Console.WriteLine("да");
+else Console.WriteLine("нет");
